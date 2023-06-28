246e8f75b034011779b970acd1a79fd624a20da3
j03-dev <24nomeniavo@gmail.com>
2023-06-28 14:11:54 +0300

    Replate rusqlite with sqlx

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,69 +1,66 @@
 use std::env::var;
 
 use dotenv::dotenv;
-use rusqlite::Connection;
+use sqlx::{Pool, Row, Sqlite, SqlitePool};
 
-fn database_connection() -> Connection {
+async fn database_connection() -> Pool<Sqlite> {
     dotenv().ok();
     let database_url =
-        var("DATABASE").expect("ckeck your .env file \n pls spécifie your database name");
-    Connection::open(database_url).expect("failed to open database")
+        var("DATABASE").expect("check your .env file \n pls spécifie your database name");
+    SqlitePool::connect(&database_url)
+        .await
+        .expect("failed to open database")
 }
 
 pub struct User {
-    pub connection: Connection,
-}
-
-impl Default for User {
-    fn default() -> Self {
-        Self {
-            connection: database_connection(),
-        }
-    }
+    pub connection: Pool<Sqlite>,
 }
 
 impl User {
-    fn is_already_exist(&self, facebook_user: &str) -> bool {
-        let sql = "select * from user where facebook_user_id=?";
-        self.connection
-            .query_row(sql, [facebook_user], |_row| Ok(()))
-            .is_ok()
-    }
-
-    pub fn create(&self, facebook_user_id: &str) -> bool {
-        if !self.is_already_exist(facebook_user_id) {
-            let sql = "insert into user (facebook_user_id, action) values (?, ?)";
-            self.connection
-                .execute(sql, [facebook_user_id, "/"])
-                .is_ok()
-        } else {
-            false
+    pub async fn new() -> Self {
+        Self {
+            connection: database_connection().await,
         }
     }
 
-    pub fn set_action(&self, facebook_user_id: &str, action: &str) -> bool {
+    pub async fn create(&self, facebook_user_id: &str) -> bool {
+        let sql = "insert into user (facebook_user_id, action) values (?, ?)";
+        sqlx::query(sql)
+            .bind(facebook_user_id)
+            .execute(&self.connection)
+            .await
+            .is_ok()
+    }
+
+    pub async fn set_action(&self, facebook_user_id: &str, action: &str) -> bool {
         let sql = "update user set action=? where facebook_user_id=?";
-        self.connection
-            .execute(sql, [action, facebook_user_id])
+        sqlx::query(sql)
+            .bind(action)
+            .bind(facebook_user_id)
+            .execute(&self.connection)
+            .await
             .is_ok()
     }
 
-    pub fn get_action(&self, facebook_user_id: &str) -> Option<String> {
+    pub async fn get_action(&self, facebook_user_id: &str) -> Option<String> {
         let sql = "select action from user where facebook_user_id=?";
-        if let Ok(action) = self
-            .connection
-            .query_row(sql, [facebook_user_id], |row| Ok(row.get(0).unwrap()))
+        match sqlx::query(sql)
+            .bind(facebook_user_id)
+            .fetch_one(&self.connection)
+            .await
         {
-            Some(action)
-        } else {
-            None
+            Ok(row) => Some(row.get(0)),
+            Err(_) => None,
         }
     }
 
-    pub fn reset_action(&self, facebook_user_id: &str) -> bool {
+    pub async fn reset_action(&self, facebook_user_id: &str) -> bool {
         let sql = "update set action=? where faceboook_user_id=?";
-        self.connection
-            .execute(sql, ["/", facebook_user_id])
+        sqlx::query(sql)
+            .bind("/")
+            .bind(facebook_user_id)
+            .execute(&self.connection)
+            .await
             .is_ok()
     }
 }

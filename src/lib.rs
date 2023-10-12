0c5cdbd5ad36f18035d5e0fbb8587b7a487fdb60
j03-dev <24nomeniavo@gmail.com>
2023-10-12 11:43:14 +0300

    add main directly on the russenger_app

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,12 @@ macro_rules! register_action {
 #[macro_export]
 macro_rules! russenger_app {
     ($($path:expr => $action:expr),* $(,)?) => {{
+        #[macro_use]
+        extern crate rocket;
+
         use std::str::FromStr;
+        use std::error::Error;
+
         use rocket_cors::{AllowedHeaders, AllowedMethods, AllowedOrigins};
 
         use russenger::core::action::ACTION_REGISTRY;
@@ -29,31 +34,36 @@ macro_rules! russenger_app {
         };
         use russenger::register_action;
 
-        $(register_action!($path, $action);)*
 
-        let allowed_origins = AllowedOrigins::all();
-        let allowed_methods: AllowedMethods = ["Get", "Post"]
-            .iter()
-            .map(|s| FromStr::from_str(s).unwrap())
-            .collect();
+        #[rocket::main]
+        async fn main() -> Result<(), Box<dyn Error>> {
 
-        let cors = rocket_cors::CorsOptions {
-            allowed_origins,
-            allowed_methods,
-            allowed_headers: AllowedHeaders::all(),
-            allow_credentials: true,
-            ..Default::default()
+            $(register_action!($path, $action);)*
+
+            let allowed_origins = AllowedOrigins::all();
+            let allowed_methods: AllowedMethods = ["Get", "Post"]
+                .iter()
+                .map(|s| FromStr::from_str(s).unwrap())
+                .collect();
+
+            let cors = rocket_cors::CorsOptions {
+                allowed_origins,
+                allowed_methods,
+                allowed_headers: AllowedHeaders::all(),
+                allow_credentials: true,
+                ..Default::default()
+            }
+            .to_cors()?;
+
+            rocket::build()
+                .attach(cors)
+                .manage(AppState::init().await)
+                .mount("/", routes![webhook_verify, webhook_core])
+                .register("/", catchers![page_not_found, server_panic])
+                .launch()
+                .await?;
+
+            Ok(())
         }
-        .to_cors()?;
-
-        rocket::build()
-            .attach(cors)
-            .manage(AppState::init().await)
-            .mount("/", routes![webhook_verify, webhook_core])
-            .register("/", catchers![page_not_found, server_panic])
-            .launch()
-            .await?;
-
-        Ok(())
     }};
 }

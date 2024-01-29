130d4343c4432713f06c28b0f3a6b4485528ffbb
j03-dev <24nomeniavo@gmail.com>
2024-01-29 16:13:18 +0300

    refactoring some thing on Res struct

--- src/core/response.rs
+++ src/core/response.rs
@@ -9,7 +9,7 @@ impl Res {
         let api = env::var("API").expect("API not found on .env file");
         let page_access_token =
             env::var("PAGE_ACCESS_TOKEN").expect("PAGE_ACCESS_TOKEN not found on .env file");
-        let facebook_api = format!("{}{}", api, page_access_token);
+        let facebook_api = format!("{api}{page_access_token}");
         reqwest::Client::new()
             .post(&facebook_api)
             .json(&value)

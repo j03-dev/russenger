fbcc9b26bb21c3bd391215b60adfcf6a4a54584a
j03-dev <24nomeniavo@gmail.com>
2024-02-24 20:39:42 +0300

    change on coreapp

--- src/core/response.rs
+++ src/core/response.rs
@@ -12,7 +12,7 @@ pub struct Res;
 
 impl Res {
     pub async fn send<T: Serialize>(&self, value: T) -> SendResult {
-        let api = "https://graph.facebook.com/v16.0/me/messages?access-token=";
+        let api = "https://graph.facebook.com/v19.0/me/messages?access-token=";
         let page_access_token =
             env::var("PAGE_ACCESS_TOKEN").expect("PAGE_ACCESS_TOKEN not found on .env file");
         let facebook_api = format!("{api}{page_access_token}");

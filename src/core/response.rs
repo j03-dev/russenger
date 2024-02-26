c1b2cab5de1532e788c25c55c45162ed92d77d88
j03-dev <24nomeniavo@gmail.com>
2024-02-26 21:42:25 +0300

    add get version of facebook_api on response

--- src/core/response.rs
+++ src/core/response.rs
@@ -12,10 +12,12 @@ pub struct Res;
 
 impl Res {
     pub async fn send<T: Serialize>(&self, value: T) -> SendResult {
-        let api = "https://graph.facebook.com/v19.0/me/messages?access_token=";
+        let version = env::var("API_VERSION").unwrap_or("15.0".into());
         let page_access_token =
             env::var("PAGE_ACCESS_TOKEN").expect("PAGE_ACCESS_TOKEN not found on .env file");
-        let facebook_api = format!("{api}{page_access_token}");
+        let facebook_api = format!(
+            "https://graph.facebook.com/{version}/me/messages?access_token={page_access_token}"
+        );
         match reqwest::Client::new()
             .post(&facebook_api)
             .json(&value)

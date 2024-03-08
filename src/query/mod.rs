056c4850b82faac445a618ebc4a33273b8ac229e
j03-dev <24nomeniavo@gmail.com>
2024-03-08 07:26:16 +0300

    fix missing feature on sql

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -135,3 +135,59 @@ impl Query {
         }
     }
 }
+
+#[cfg(test)]
+mod querytest {
+    use dotenv::dotenv;
+
+    use crate::core::request::Req;
+    use crate::core::response::Res;
+    use crate::Action;
+
+    use super::Query;
+
+    static USER_TEST: &'static str = "TEST";
+
+    struct TestAction;
+
+    #[crate::async_trait]
+    impl Action for TestAction {
+        fn path(&self) -> String {
+            "TestAction".into()
+        }
+
+        async fn execute(&self, _: Res, _: Req) {}
+    }
+
+    #[crate::async_test]
+    async fn test_migrate() {
+        dotenv().ok();
+        let query = Query::new().await;
+        let result = query.migrate().await;
+        assert_eq!(true, result);
+    }
+
+    #[crate::async_test]
+    async fn test_create_user() {
+        dotenv().ok();
+        let query = Query::new().await;
+        let result = query.create(USER_TEST).await;
+        assert_eq!(true, result);
+    }
+
+    #[rocket::async_test]
+    async fn test_set_action() {
+        dotenv().ok();
+        let query = Query::new().await;
+        let result = query.set_action(USER_TEST, TestAction).await;
+        assert_eq!(true, result);
+    }
+
+    #[rocket::async_test]
+    async fn get_action() {
+        dotenv().ok();
+        let query = Query::new().await;
+        let result = query.get_action(USER_TEST).await;
+        assert_eq!(Some("TestAction".to_string()), result);
+    }
+}

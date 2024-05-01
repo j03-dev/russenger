408825b0c5ee215167add3102c538072c0780a70
j03-dev <24nomeniavo@gmail.com>
2024-05-01 16:14:50 +0300

    Add documentation for Button enum in button.rs

--- src/response_models/button.rs
+++ src/response_models/button.rs
@@ -2,6 +2,57 @@ use rocket::serde::json::{json, Value};
 
 use super::payload::Payload;
 
+/// `Button` is an enum that represents different types of buttons that can be used in a Messenger conversation.
+///
+/// # Variants
+///
+/// * `AccountUnlink` - Represents an account unlink button.
+/// * `AccountLink { url: &'b str }` - Represents an account link button. The `url` field is the URL to be opened when the button is clicked.
+/// * `WebUrl { title: &'b str, url: &'b str }` - Represents a web URL button. The `title` field is the title of the button, and the `url` field is the URL to be opened when the button is clicked.
+/// * `Postback { title: &'b str, payload: Payload }` - Represents a postback button. The `title` field is the title of the button, and the `payload` field is the payload to be sent back to the server when the button is clicked.
+/// * `PhoneNumber { title: &'b str, payload: Payload }` - Represents a phone number button. The `title` field is the title of the button, and the `payload` field is the phone number to be dialed when the button is clicked.
+///
+/// # Examples
+///
+/// Creating a `Button` and converting it to a `Value`:
+///
+/// ```rust
+/// use russenger::response_models::data::Data;
+/// use russenger::response_models::payload::Payload;
+/// use russenger::response_models::button::Button;
+// Creating an AccountUnlink button
+/// let account_unlink_button = Button::AccountUnlink;
+///
+/// // Creating an AccountLink button
+/// let account_link_button = Button::AccountLink {
+///     url: "https://www.facebook.com/your_account_page",
+/// };
+///
+/// // Creating a WebUrl button
+/// let web_url_button = Button::WebUrl {
+///     title: "Visit Website",
+///     url: "https://example.com",
+/// };
+///
+/// // Creating a Postback button
+/// let postback_button = Button::Postback {
+///     title: "Click me",
+///     payload: Payload::new(HelloWorld, Some(Data::new("HelloWorld", None))),
+/// };
+///
+/// // Creating a PhoneNumber button
+/// let phone_number_button = Button::PhoneNumber {
+///     title: "Call me",
+///     payload: Payload::new(HelloWorld, Some(Data::new("<PhoneNumber>", None))),
+/// };
+/// 
+/// use russenger::prelude::*;
+/// 
+/// create_action!(HelloWorld, |res: Res, req: Req| async move {
+///     let payload: String = req.data.get_value();
+///     res.send(TextModel::new(&req.user, &payload)).await;
+/// });
+/// ```
 #[derive(Clone, Debug)]
 pub enum Button<'b> {
     AccountUnlink,

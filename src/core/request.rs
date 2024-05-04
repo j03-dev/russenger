e3a7dcf4fec39212fd9b56b8820993e87919f31a
j03-dev <24nomeniavo@gmail.com>
2024-05-04 09:34:54 +0300

    Update documentation for `Data` struct in `request.rs`

--- src/core/request.rs
+++ src/core/request.rs
@@ -41,7 +41,51 @@ pub struct Req {
     /// create_action!(NextAction, |res: Res, req: Req| async move {});
     /// ```
     pub query: Query,
+
+    /// The `Data` struct represents a data object with a value and an optional page.
+    ///
+    /// This struct is used to store and manipulate data. It contains a `value` field, which is a serialized JSON string, and a `page` field, which is an optional `Page` struct.
+    ///
+    /// # Fields
+    ///
+    /// * `value`: The value of the data. This is a serialized JSON string.
+    /// * `page`: The page of the data. This is an optional `Page` struct.
+    ///
+    /// # Methods
+    ///
+    /// * `new`: This method creates a new `Data`. It takes a value and an optional page as arguments, serializes the value into a JSON string, and returns a `Data` with the serialized string and the page.
+    /// * `get_value`: This method deserializes the value of the data into a specified type. It returns the deserialized value if it exists, or the default value of the type if it doesn't.
+    /// * `get_page`: This method returns the page of the data.
+    ///
+    /// # Examples
+    ///
+    /// Creating a new `Data` and using it to get the value and the page:
+    ///
+    /// ```rust
+    /// use russenger::response_models::data::{Data, Page};
+    ///
+    /// let data = Data::new("value", Some(Page::default()));
+    /// let value: String = data.get_value();
+    /// let page = data.get_page();
+    /// ```
     pub data: Data,
+
+    /// The `host` field represents the host name or IP address of the server that the request is being sent to.
+    ///
+    /// This field is used to specify the server that the request should be sent to. It is a `String` that contains the host name or IP address of the server.
+    ///
+    /// # Examples
+    ///
+    /// Creating a new `Req` and setting the `host` field:
+    ///
+    /// ```rust
+    /// use russenger::prelude::*;
+    ///
+    /// create_action!(Main, |res: Res, req: Req| async move {
+    ///    let media = MediaModel::new(&req.user, "image", &format!("{host}/image.jpg", host = req.host));
+    ///    res.send(media).await;
+    /// ));
+    /// ````
     pub host: String,
 }
 

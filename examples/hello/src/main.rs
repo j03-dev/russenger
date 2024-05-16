5aa68b377565c780053e07ff2dc43a4c7719a468
j03-dev <24nomeniavo@gmail.com>
2024-05-16 11:22:00 +0300

    chore: Update dependencies and remove unused code

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -2,6 +2,11 @@ use russenger::prelude::*;
 
 #[action]
 async fn Main(res: Res, _req: Req) {
+    let message: String = req.data.get_value();
+    if message.to_lowercase() == "hello" {
+        res.send(TextModel::new(&req.user, "Hello, welcome !"))
+            .await;
+    }
     res.send(GetStartedModel::new(Payload::new(Start, None)))
         .await;
 }
@@ -11,7 +16,7 @@ async fn Start(res: Res, req: Req) {
     res.send(PersistentMenuModel::new(
         &req.user,
         vec![Button::Postback {
-            title: "hello world",
+            title: "hello world".to_string(),
             payload: Payload::new(HelloWorld, None),
         }],
     ))
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,52 @@
+//! # Russenger Library
+//!
+//! The Russenger library provides a set of modules and macros to help you build bots using the Russenger bot framework.
+//!
+//! ## Modules
+//!
+//! - `cli`: This module provides command-line interface utilities.
+//! - `core`: This module contains the core functionalities of the Russenger bot framework.
+//! - `prelude`: This module re-exports important traits and structs for convenience.
+//! - `query`: This module provides utilities for handling queries.
+//! - `response_models`: This module contains models for different types of responses.
+//!
+//! ## Macro
+//!
+//! - `action`: This proc macro is used to define an action.
+//!
+//! ## Deprecated
+//!
+//! - `create_action`: This macro is deprecated. Please use the `#[action]` proc macro instead. It was used to create a new action that sends a greeting message when the user input is "Hello".
+//!
+//! ## Examples
+//!
+//! Creating a new action that sends a greeting message when the user input is "Hello":
+//!
+//! ```rust
+//! use russenger::create_action;
+//! use russenger::prelude::*;
+//!
+//! create_action!(Main, |res: Res, req: Req| async move {
+//!     let message: String = req.data.get_value();
+//!     if message == "Hello" {
+//!         res.send(TextModel::new(&req.user, "Hello, welcome to our bot!")).await;
+//!     }
+//! });
+//! ```
+//!
+//! Creating an action using the new `#[action]` proc macro:
+//!
+//! ```rust
+//! use russenger::prelude::*;
+//!
+//! #[action]
+//! async fn Main(res: Res, req: Req) {
+//!     let message: String = req.data.get_value();
+//!     if message == "Hello" {
+//!         res.send(TextModel::new(&req.user, "Hello, welcome to our bot!")).await;
+//!     }
+//! }
+//! ```
 pub use actix_web::main;
 pub use async_trait::async_trait;
 pub mod cli;

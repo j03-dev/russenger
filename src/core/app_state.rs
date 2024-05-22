cb661af8bf498204ad97570126ffd3565d5c25f7
j03-dev <24nomeniavo@gmail.com>
2024-05-22 14:10:56 +0300

    the documentation for app_state

--- src/core/app_state.rs
+++ src/core/app_state.rs
@@ -1,5 +1,16 @@
+//! The `app_state` module contains the `AppState`
+//!
+//! The `AppState` struct is use to store the state of the application.
+//! query:  `Query` is use to query the database
 use crate::query::Query;
 
+/// # App State
+///
+/// This module contains the `AppState` struct which is used to store the state of the application.
+///
+/// # Fields
+///
+/// * `query`: A `Query` that represents the query made by the user.
 #[derive(Clone)]
 pub struct AppState {
     pub query: Query,

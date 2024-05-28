ec23a63d0000c6ced090761cc24537f589c67a42
j03-dev <24nomeniavo@gmail.com>
2024-05-29 00:39:44 +0300

    chore: Refactor query module and add new methods

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -74,18 +74,42 @@ pub struct Query {
     pub conn: DatabaseConnection,
 }
 
+/// Represents a query object used for database operations.
 impl Query {
+    /// Creates a new Query object.
+    ///
+    /// # Returns
+    ///
+    /// Returns a Query object if the connection is successfully established.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the connection cannot be established.
     pub async fn new() -> Self {
         match establish_connection().await {
             Ok(conn) => Self { conn },
-            Err(err) => panic!("Can't estabilish the connection {err:?}"),
+            Err(err) => panic!("Can't establish the connection {err:?}"),
         }
     }
 
+    /// Runs database migrations.
+    ///
+    /// # Returns
+    ///
+    /// Returns `true` if the migrations are successful, `false` otherwise.
     pub async fn migrate(&self) -> bool {
         Migrator::up(&self.conn, None).await.is_ok()
     }
 
+    /// Creates a new record in the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `user_id` - The ID of the user.
+    ///
+    /// # Returns
+    ///
+    /// Returns `true` if the record is successfully created, `false` otherwise.
     pub async fn create(&self, user_id: &str) -> bool {
         ActiveModel {
             senderid: Set(user_id.to_owned()),
@@ -96,6 +120,36 @@ impl Query {
         .is_ok()
     }
 
+    /// Sets the action for a user.
+    ///
+    /// # Arguments
+    ///
+    /// * `user_id` - The ID of the user.
+    /// * `action` - The action to set.
+    ///
+    /// # Returns
+    ///
+    /// Returns `true` if the action is successfully set, `false` otherwise.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use russenger::prelude::*;
+    ///
+    /// #[action]
+    /// async fn Main(res: Res, req: Req) {
+    ///     res.send(TextModel::new(&req.user, "What is your name: ")).await;
+    ///     req.query.set_action(&req.user, GetUserInput).await;
+    /// }
+    ///
+    /// #[action]
+    /// async fn GetUserInput(res: Res, req: Req) {
+    ///     let username: String = req.data.get_value();
+    ///     res.send(TextModel::new(&req.user, &format!("Hello : {username}"))).await;
+    /// }
+    /// 
+    /// russenger_app!(Main, GetUserInput);
+    /// ```
     pub async fn set_action<A: Action>(&self, user_id: &str, action: A) -> bool {
         if let Ok(Some(user)) = User::find_by_id(user_id).one(&self.conn).await {
             ActiveModel {
@@ -110,6 +164,15 @@ impl Query {
         }
     }
 
+    /// Retrieves the action for a user.
+    ///
+    /// # Arguments
+    ///
+    /// * `user_id` - The ID of the user.
+    ///
+    /// # Returns
+    ///
+    /// Returns the action as an `Option<String>`. Returns `None` if the user is not found.
     pub async fn get_action(&self, user_id: &str) -> Option<String> {
         if let Ok(Some(user)) = User::find_by_id(user_id).one(&self.conn).await {
             Some(user.action.to_owned())
